43392e39295f01c450eb79a49c09eff41ee6770b
Vivek Bansal <bansalvivek79@gmail.com>
2015-07-12 17:58:52 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,7 +8,6 @@
 	a[0] , b[0] , a[1] , b[1] , a[2] , b[2] these are the three point coordinates of triangle 
 	and px , py - coordinates of the point to be checked .
 */
-int fun(i
 int fun(int x, int y , int x1, int y1, int x2 ,int y2)
 {
 	return (y1-y)*(x2-x1) + (y2-y1)*(x-x1);
@@ -21,3 +20,4 @@ bool pointinsidetriangle()
 	b3 = fun(px , py , a[2] , b[2] , a[0] , b[0]) < 0 ;
 	return (b1==b2 && b2==b3);
 }
+/* now call the pointinsidetriangle function to check. */
