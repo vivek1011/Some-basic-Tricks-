6b184d6154db43d6250607076cf3b3ed0409d10b
Vivek Bansal <bansalvivek79@gmail.com>
2015-10-23 14:47:55 +0530

    Create inverseangle.cpp

---
+++ inverseangle.cpp
@@ -0,0 +1,4 @@
+how to calculate inverse of any value and return it in degrees
+for example calculate tan-1(b/a) and return it in degrees
+ans = atan2(b,a)*180.000000/(M_PI) ; 
+here atan2() is the funstion name ; 
